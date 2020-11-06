b8cc2a3723ff22d9d90b86fd06a9918a443332eb
AkJadoon19 <73095121+AkJadoon19@users.noreply.github.com>
2020-11-06 10:46:09 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to abdullah khan 2017023.
+          Edit and delete <code>src/App.js</code> and save to abdullah khan 2017023.
         </p>
         <a
           className="App-link"
